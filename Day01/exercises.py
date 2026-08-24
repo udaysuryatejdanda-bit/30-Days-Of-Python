3ea596f0f70e7e1710367770a2857fd23e3544e7
udaysuryatejdanda-bit <udaysuryatejdanda@gmail.com>
2026-08-24 16:54:08 +0530

    DAY -1 (2/3)

---
+++ Day01/exercises.py
@@ -0,0 +1,24 @@
+#Level 1
+#exercise 1
+# Python version: 3.13.15
+#exercise 2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+#exercise 3
+print("Uday surya tej\nDanda\nINDIA\nI love 30 days python challenge")
+#exercise 4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Uday Surya Tej"))
+print(type("danda"))
+print(type("INDIA"))
+#Level 2
+#exercise 1
